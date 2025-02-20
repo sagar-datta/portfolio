22ed554e1d93113d5af84b89fe96b7c59a013f9d
sagar-datta <s@gar.jp.net>
2025-02-20 19:45:21 +0800

    Migrate ESLint configuration to flat config and consolidate rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,8 +16,21 @@ const eslintConfig = [
       quotes: ["error", "single"],
       semi: ["error", "always"],
       indent: ["error", 2],
-      "max-len": ["error", { code: 80 }],
       "comma-dangle": ["error", "always-multiline"],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "@typescript-eslint/no-empty-interface": [
+        "error",
+        {
+          allowSingleExtends: true,
+        },
+      ],
     },
   },
 ];
