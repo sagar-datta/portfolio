b6c507b985a40ead519595926d474809ce059791
sagar-datta <s@gar.jp.net>
2025-02-20 14:41:19 +0800

    Improve header title alignment and responsiveness

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -6,9 +6,11 @@ export const Header = ({ title }: IHeaderProps) => {
   return (
     <header className="sticky top-0 z-10 backdrop-blur-sm bg-primary/75 dark:bg-primary-dark/75">
       <div className="max-w-7xl mx-auto p-6">
-        <h1 className="text-2xl font-bold text-center lg:text-left origin-left select-none [transform:scale(1.05,1)]">
-          {title}
-        </h1>
+        <div className="flex justify-center lg:justify-start">
+          <h1 className="text-2xl font-bold select-none [transform:scale(1.05,1)] origin-center lg:origin-left">
+            {title}
+          </h1>
+        </div>
       </div>
     </header>
   );
