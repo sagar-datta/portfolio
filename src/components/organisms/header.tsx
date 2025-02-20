6119c1b4ff9ce664c76728fcd05e8b309a7143e9
sagar-datta <s@gar.jp.net>
2025-02-20 13:16:58 +0800

    Enhance header title with subtle scale and select-none styling

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -6,7 +6,7 @@ export const Header = ({ title }: IHeaderProps) => {
   return (
     <header className="sticky top-0 z-10 backdrop-blur-sm bg-primary/75 dark:bg-primary-dark/75">
       <div className="max-w-7xl mx-auto p-6">
-        <h1 className="text-2xl font-bold tracking-tight text-center lg:text-left">
+        <h1 className="text-2xl font-bold text-center lg:text-left origin-left select-none [transform:scale(1.05,1)]">
           {title}
         </h1>
       </div>
