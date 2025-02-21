bf4570b8c795ccaa488e407fb57cb3b24bdaa923
sagar-datta <s@gar.jp.net>
2025-02-21 17:38:08 +0800

    refactor: Adjust Header bottom margin for large screen responsiveness

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -8,7 +8,7 @@ interface IHeaderProps {
 
 export const Header = ({ name, profession }: IHeaderProps) => {
   return (
-    <header className="sticky top-4 z-10 mx-6 md:mx-4 mb-6">
+    <header className="sticky top-4 z-10 mx-6 md:mx-4 mb-6 lg:mb-0">
       <div className="max-w-4xl mx-auto bg-glass-button dark:bg-glass-button-dark backdrop-blur-glass rounded-2xl">
         <div className="relative flex items-center p-1.5 md:px-8 md:py-4">
           {/* Left section with icon and name */}
