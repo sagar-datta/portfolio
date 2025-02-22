8ab69f440ca9d71dfd432359debcf0b18caa24c4
sagar-datta <s@gar.jp.net>
2025-02-22 14:17:16 +0800

    refactor: Remove client directives and extract Projects content component

--- src/components/molecules/Projects.tsx
+++ src/components/molecules/Projects.tsx
@@ -1,25 +1,29 @@
-"use client";
-
 import { PageTransition } from "@/components/atoms/PageTransition";
 import { GridItem } from "@/components/molecules/GridItem";
 import { GRID_ITEMS } from "@/utils/constants";
 
+const ProjectsContent = () => {
+  return (
+    <div className="flex flex-col md:flex-row md:flex-wrap gap-6 justify-center items-center md:items-stretch">
+      {GRID_ITEMS.map((item) => (
+        <GridItem
+          key={item.id}
+          title={item.title}
+          descriptionHeading={item.descriptionHeading}
+          descriptionExtended={item.descriptionExtended}
+          url={item.url}
+          imagePath={item.imagePath}
+          techStack={item.techStack}
+        />
+      ))}
+    </div>
+  );
+};
+
 export const Projects = () => {
   return (
     <PageTransition>
-      <div className="flex flex-col md:flex-row md:flex-wrap gap-6 justify-center items-center md:items-stretch">
-        {GRID_ITEMS.map((item) => (
-          <GridItem
-            key={item.id}
-            title={item.title}
-            descriptionHeading={item.descriptionHeading}
-            descriptionExtended={item.descriptionExtended}
-            url={item.url}
-            imagePath={item.imagePath}
-            techStack={item.techStack}
-          />
-        ))}
-      </div>
+      <ProjectsContent />
     </PageTransition>
   );
 };
